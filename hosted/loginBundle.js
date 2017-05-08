7d72390164bdcbe6bcba764a4bd2254351484f33
AJKaufman <ak3012@rit.edu>
2017-05-08 13:47:26 -0400

    added a temp styles.css, setup some paths

--- hosted/loginBundle.js
+++ hosted/loginBundle.js
@@ -4,6 +4,32 @@
 var username = void 0;
 var pass = void 0;
 
+// using code from DomoMaker E by Aidan Kaufman
+var handleError = function handleError(message) {
+  $("#errorMessage").text(message);
+};
+
+var redirect = function redirect(response) {
+  window.location = response.redirect;
+};
+
+var sendAjax = function sendAjax(type, action, data, success) {
+  $.ajax({
+
+    cache: false,
+    type: type,
+    url: action,
+    data: data,
+    dataType: 'json',
+    success: success,
+    error: function error(xhr, status, _error) {
+
+      var messageObj = JSON.parse(xhr.responseText);
+      handleError(messageObj.error);
+    }
+  });
+};
+
 var handleLogin = function handleLogin(e) {
   e.preventDefault();
 
