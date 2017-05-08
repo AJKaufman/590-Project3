bdde4b9be69a65d020034f428303cad024930a11
AJKaufman <ak3012@rit.edu>
2017-05-08 13:49:57 -0400

    commented out Game.js files because idk if I need them

--- src/controllers/Game.js
+++ src/controllers/Game.js
@@ -1,15 +1,15 @@
-// using code from DomoMaker E
-const models = require('../models');
-
-const Game = models.Game;
-
-const makerPage = (req, res) => {
-  Game.GameModel.findByOwner(req.session.account._id, (err, docs) => {
-    if (err) {
-      console.log(err);
-      return res.status(400).json({ error: 'An error occurred' });
-    }
-
-    return res.render('app', { csrfToken: req.csrfToken(), cats: docs });
-  });
-};
+// // using code from DomoMaker E
+// const models = require('../models');
+//
+// const Game = models.Game;
+//
+// const makerPage = (req, res) => {
+//  Game.GameModel.findByOwner(req.session.account._id, (err, docs) => {
+//    if (err) {
+//      console.log(err);
+//      return res.status(400).json({ error: 'An error occurred' });
+//    }
+//
+//    return res.render('app', { csrfToken: req.csrfToken(), cats: docs });
+//  });
+// };
--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ const router = (app) => {
   app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
   app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
   app.get('/logout', mid.requiresLogin, controllers.Account.logout);
-  //app.get('/maker', mid.requiresLogin, controllers.Game.make);
+  // app.get('/maker', mid.requiresLogin, controllers.Game.make);
   app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
 };
 
